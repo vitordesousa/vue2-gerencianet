d00819638d51f3b77ce7f1dda11191db05c9ade8
Vitor de Sousa <vitordessilva@gmail.com>
2021-11-02 22:15:26 -0300

    base inicial em bootstrap para visualização dos dados

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Gerencianet />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Gerencianet from './components/Gerencianet.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Gerencianet
+
   }
 }
 </script>
@@ -21,8 +23,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
+@import '~bootstrap/dist/css/bootstrap.css'
 </style>
---
+++ src/components/Gerencianet.vue
@@ -0,0 +1,86 @@
+<template>
+	<div class="container">
+		<div class="row" v-if="messages != ''">
+			<div class="col-lg-12 card mb-5">
+				<h2 class="py-3 px-2">Mensagens do sistema</h2>
+				<ul class="list-group list-group-flush">
+					<li class="list-group-item" v-for="message in messages" :key="message">{{message}}</li>
+				</ul>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="mb-3">
+					<label for="payee_code" class="form-label">Payee Code (<a href="https://cdn.discordapp.com/attachments/652136190006525955/809138574549188618/identificador-conta.jpg">Veja onde</a>)</label>
+					<input type="text" class="form-control" id="payee_code" v-model="payee_code" placeholder="Digite o Código da sua conta">
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-6">
+				<div class="mb-3">
+					<label for="brand" class="form-label">Bandeira do Cartão</label>
+					<input type="text" class="form-control" id="brand" v-model="cardData.brand" placeholder="Digite a Bandeira do Cartão">
+				</div>
+			</div>
+			<div class="col-lg-6">
+				<div class="mb-3">
+					<label for="number" class="form-label">Número do Cartão</label>
+					<input type="text" class="form-control" id="number" v-model="cardData.number" placeholder="Digite o Número do Cartão">
+				</div>
+			</div>
+			<div class="col-lg-6">
+				<div class="mb-3">
+					<label for="cvv" class="form-label">CVV</label>
+					<input type="text" class="form-control" id="cvv" v-model="cardData.brand" placeholder="Digite o CVV do Cartão" maxlength="4">
+				</div>
+			</div>
+			<div class="col-lg-6">
+				<div class="mb-3">
+					<label for="expiration_month" class="form-label">Mês de Expiração do Cartão</label>
+					<input type="text" class="form-control" id="expiration_month" v-model="cardData.expiration_month" placeholder="Digite o Mês de Expiração do Cartão" maxlength="2">
+				</div>
+			</div>
+			<div class="col-lg-6">
+				<div class="mb-3">
+					<label for="expiration_year" class="form-label">Número do Cartão</label>
+					<input type="text" class="form-control" id="expiration_year" v-model="cardData.expiration_year" placeholder="Digite o Mês de Expiração do Cartão" maxlength="4">
+				</div>
+			</div>
+			<div class="col-lg-6 pt-4">
+				<p class="alert alert-info">*Os dados do cartão são fictícios e utilizados na documentação da Gerencianet</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="d-grid gap-2">
+					<button class="btn btn-primary" id="btnSend" @click="gerarToken()">Gerar token</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		data(){
+			return {
+				messages			:	[],
+				payee_code			:	'',
+				cardData			:	{
+					brand				:	'visa', // bandeira do cartão
+					number				:	'4012001038443335', // número do cartão
+					cvv					:	'123', // código de segurança
+					expiration_month	:	'05', // mês de vencimento
+					expiration_year		:	'2021' // ano de vencimento
+				}
+			}
+		},
+		methods : {
+			gerarToken(){
+				
+			}
+		}
+
+	}
+</script>
